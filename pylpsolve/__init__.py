5766e1a799519f469488c61b0c65e0dc98be2c90
Robert Estelle <robert@getpreo.com>
2016-03-13 04:09:52 -0400

    Raise specific error exception types.

--- pylpsolve/__init__.py
+++ pylpsolve/__init__.py
@@ -15,4 +15,9 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
-from pylpsolve import LP, LPException
+from pylpsolve import (
+    LP, LPException, LPSuboptimalException, LPInfeasibleException,
+    LPUnboundedException, LPModelDegenerateException,
+    LPNumericalFailureException, LPUserAbort, LPTimeout, LPProcFail,
+    LPProcBreak
+)
